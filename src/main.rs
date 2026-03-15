1440085f434bd0559bd4ace96c19c39156f17402
itssidhere <sidjha0001@gmail.com>
2026-03-15 15:17:56 -0600

    feat: refactor main function to implement table creation and row insertion

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
-use gatidb::btree::BTree;
+use gatidb::{
+    btree::BTree,
+    buffer::BufferPool,
+    disk::{self, DiskManager},
+    table::{Column, DataType, Schema, Table, Value},
+};
 
 fn main() {
-    let mut tree = BTree::new(2);
+    let schema = Schema {
+        columns: vec![
+            Column {
+                name: "id".to_string(),
+                data_type: DataType::Int,
+            },
+            Column {
+                name: "comment".to_string(),
+                data_type: DataType::Varchar(128),
+            },
+        ],
 
-    for i in 0..20 {
-        tree.insert(i, format!("value_{}", i));
+        primary_key: 0,
+    };
+    let disk_manager = DiskManager::new("disk_mgr");
+    let pool = BufferPool::new(disk_manager);
+    let mut table = Table::new("jobs", schema, pool, 64);
+
+    table.insert_row(&[Value::Int(1), Value::Varchar("fix the bug".to_string())]);
+    table.insert_row(&[Value::Int(2), Value::Varchar("deploy to prod".to_string())]);
+
+    if let Some(row) = table.get_row(1) {
+        println!("{:?}", row)
     }
 
-    println!("{:?}", tree.search(0));
-    println!("{:?}", tree.search(10));
-    println!("{:?}", tree.search(19));
-    println!("{:?}", tree.search(99));
+    table.flush();
 }
